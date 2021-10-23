4e2b99b26dad3dcc2ad0c5803c2d7e16b5e72e86
Swapnil-Patil17 <83502985+Swapnil-Patil17@users.noreply.github.com>
2021-10-23 20:04:35 +0530

    Update Statistics.java

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -6,5 +6,31 @@ public class Statistics
 {
     public static Stats getStatistics(List<___> numbers) {
         //implement the computation of statistics here
+        public float sum=0;
+		public float min=numbers[0];
+		public float max=numbers[0]; 
+		public float average=0;
+		
+		if(numbers.length != 0){
+		
+		  for(int i=0;i<numbers.length;i++){
+			  sum +=numbers[i];
+			  
+			  if(max > numbers[i]){
+				  max=numbers[i];
+			  }
+			  
+			  if(min < numbers[i]){
+				  min=numbers[i];
+			  }
+		  }
+		  
+		  average=sum/numbers.length;
+		}
+		else{
+			average=NaN;
+			min=NaN;
+			max=NaN;
+		}
     }
 }
