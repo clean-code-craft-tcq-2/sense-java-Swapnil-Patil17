10e14bf51237d7b473ef919b726ac3c4fe65c071
Swapnil-Patil17 <83502985+Swapnil-Patil17@users.noreply.github.com>
2021-10-25 11:11:54 +0530

    Update Statistics.java

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -3,9 +3,8 @@ package statisticker;
 import java.util.Collections;
 import java.util.List;
 
-public class Statistics 
-{
-    
+public class statistics {
+
   public static class Stats {
 
     Float min, max, average;
@@ -18,29 +17,29 @@ public class Statistics
   }
 
   public static Stats getStatistics(final List<Float> numberList) {
-	float sum = 0.0f;
- 	float max = numberList.get(0);
- 	float min = numberList.get(0);
-    	if (numberList.size() != 0) {
+    float sum = 0.0f;
+    float max = numberList.get(0);
+    float min = numberList.get(0);
+    if (numberList.size() != 0) {
 
-		for (int i = 0; i < numberList.size(); i++) {
-			sum += numberList.get(i);
+      for (int i = 0; i < numberList.size(); i++) {
+        sum += numberList.get(i);
 
-			if (max > numberList.get(i)) {
-			max = numberList.get(i);
-			}
+        if (max > numberList.get(i)) {
+          max = numberList.get(i);
+        }
 
-			if (min < numberList.get(i)) {
-			  min = numberList.get(i);
-			}
-		 }
+        if (min < numberList.get(i)) {
+          min = numberList.get(i);
+        }
+      }
 
-	      float average = sum / numberList.size();
-	      return new Stats(average, min, max);
-	    }
-	 else{
-		return new Stats(Float.NaN, Float.NaN, Float.NaN);
-	  }
+      float average = sum / numberList.size();
+      return new Stats(average, min, max);
+    }
+    else{
+      return new Stats(Float.NaN, Float.NaN, Float.NaN);
+    }
 
-}
+  }
 }
