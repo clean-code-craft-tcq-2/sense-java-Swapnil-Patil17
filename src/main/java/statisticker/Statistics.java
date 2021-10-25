583de72f960ad6b500d57e9304dd9265b9bb7e28
Swapnil-Patil17 <83502985+Swapnil-Patil17@users.noreply.github.com>
2021-10-25 09:55:29 +0530

    Update Statistics.java

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -7,20 +7,20 @@ import java.util.List;
 
 public class Statistics 
 {
-public static class stats {
+public static class Stats {
 
   float average;
   float min;
   float max;
 
-  public stats(final float average, final float min, final float max) {
+  public Stats(final float average, final float min, final float max) {
     this.average = average;
     this.min = min;
     this.max = max;
   }
 }
 
-  public static stats getStatistics(final List<Float> numberList) {
+  public static Stats getStatistics(final List<Float> numberList) {
     float sum = 0;
     float min = numberList.get(0);
     float max = numberList.get(0);
@@ -41,10 +41,10 @@ public static class stats {
       }
 
       average = sum / numberList.size();
-	  return new stats(average, min, max);
+	  return new Stats(average, min, max);
     }
 	else{
-		return new stats(Float.NaN, Float.NaN, Float.NaN);
+		return new Stats(Float.NaN, Float.NaN, Float.NaN);
 	}
     
   }
